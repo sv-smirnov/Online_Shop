4cc1264ff39f999fa8b18dda59eb6ca38502a6d5
chlenix8000 <cmuphob91@mail.ru>
2022-08-21 18:31:02 +0500

    Memurai connection added Cache manager reworked

--- src/main/java/com/geekbrains/spring/web/dto/OrderItemDto.java
+++ src/main/java/com/geekbrains/spring/web/dto/OrderItemDto.java
@@ -3,9 +3,11 @@ package com.geekbrains.spring.web.dto;
 import com.geekbrains.spring.web.entities.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemDto {
 
     private Long productId;
--- src/main/java/com/geekbrains/spring/web/services/CartService.java
+++ src/main/java/com/geekbrains/spring/web/services/CartService.java
@@ -1,75 +1,65 @@
 package com.geekbrains.spring.web.services;
 
-import com.geekbrains.spring.web.Constants;
 import com.geekbrains.spring.web.dto.Cart;
 import com.geekbrains.spring.web.entities.Product;
 import com.geekbrains.spring.web.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
     private final ProductsService productsService;
+    @Qualifier("test")
     private final CacheManager cacheManager;
-    @Value("${other.cache.cart}")
+    //    @Value("${other.cache.cart}")
+    @Value("${spring.cache.user.name}")
     private String CACHE_CART;
     private Cart cart;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    @Cacheable(value = "${other.cache.cart}", key = "#cartName")
-    public Cart getCurrentCart(String cartName){
+    @Cacheable(value = "Cart", key = "#cartName")
+    public Cart getCurrentCart(String cartName) {
         cart = cacheManager.getCache(CACHE_CART).get(cartName, Cart.class);
-        if(!Optional.ofNullable(cart).isPresent()){
+        if (!Optional.ofNullable(cart).isPresent()) {
             cart = new Cart(cartName, cacheManager);
             cacheManager.getCache(CACHE_CART).put(cartName, cart);
         }
         return cart;
     }
 
-    @CachePut(value = "${other.cache.cart}", key = "#cartName")
-    public Cart addProductByIdToCart(Long id, String cartName){
+    @CachePut(value = "Cart", key = "#cartName")
+    public Cart addProductByIdToCart(Long id, String cartName) {
         Cart cart = getCurrentCart(cartName);
-        if(!getCurrentCart(cartName).addProductCount(id)){
+        if (!cart.addProductCount(id)) {
             Product product = productsService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Не удалось найти продукт"));
             cart.addProduct(product);
         }
-            return cart;
+        return cart;
     }
 
-//    public void addProductByIdToCart(Long id, String cartName){
-//        if(!getCurrentCart(cartName).addProductCount(id)){
-//            Product product = productsService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Не удалось найти продукт"));
-//            Cart cart = getCurrentCart(cartName);
-//            cart.addProduct(product);
-//            cacheManager.getCache("Cart").put(cartName, cart);
-//        }
-//    }
-
-    @CachePut(value = "${other.cache.cart}", key = "#cartName")
-    public void clear(String cartName){
+    @CachePut(value = "Cart", key = "#cartName")
+    public Cart clear(String cartName) {
         Cart cart = getCurrentCart(cartName);
         cart.clear();
+        return cart;
     }
 
-//    public void clear(String cartName){
-//        Cart cart = getCurrentCart(cartName);
-//        cart.clear();
-//        cacheManager.getCache(CACHE_CART).put(cartName, cart);
-//    }
-
-    public void removeProductByIdFromCart(Long id, String cartName){
-        if(!getCurrentCart(cartName).decreaseProductCount(id)){
+    @CachePut(value = "Cart", key = "#cartName")
+    public Cart removeProductByIdFromCart(Long id, String cartName) {
+        Cart cart = getCurrentCart(cartName);
+        if (!cart.decreaseProductCount(id)) {
             Product product = productsService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Не удалось найти продукт"));
-            Cart cart = getCurrentCart(cartName);
             cart.decreaseProduct(id);
-            cacheManager.getCache("Cart").put(cartName, cart);
         }
+        return cart;
     }
-
 }
