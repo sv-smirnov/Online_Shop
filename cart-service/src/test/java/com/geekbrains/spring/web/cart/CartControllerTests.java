28b0540fa6dccab07e0bf18d3ee7d5095501a6bb
chlenix8000 <cmuphob91@mail.ru>
2022-09-05 11:06:37 +0500

    added some tests fot cart MS

--- cart-service/src/test/java/com/geekbrains/spring/web/cart/CartControllerTests.java
+++ cart-service/src/test/java/com/geekbrains/spring/web/cart/CartControllerTests.java
@@ -1,29 +1,67 @@
 package com.geekbrains.spring.web.cart;
 
 import com.geekbrains.spring.web.cart.dto.Cart;
-import com.geekbrains.spring.web.cart.dto.OrderItemDto;
 import com.geekbrains.spring.web.cart.dto.ProductDto;
 import com.geekbrains.spring.web.cart.services.CartService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CartControllerTests {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private TestRestTemplate testRestTemplate;
 
     @Autowired
     private CartService service;
 
-    @Test
-    public void getCurrentCartTest() {
-        Cart cart = restTemplate.postForObject("/api/v1/carts", "cartName", Cart.class);
-        assertThat(cart).isNotNull();
+    @Autowired
+    private MockMvc mvc;
 
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @BeforeEach
+    public void initCart() {service.clear("newCart");}
+
+    @Test
+    public void getCurrentCartTest() throws Exception {
+        Cart cart = testRestTemplate.postForObject("/api/v1/carts", "newCart", Cart.class);
+        assertThat(cart).isNotNull();
+        mvc.perform(
+                post("/api/v1/carts").contentType(MediaType.APPLICATION_JSON).content("newCart"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty());
     }
+
+    @Test
+    public void addProductToCartTest() throws Exception {
+        ProductDto product = new ProductDto(1L, "Milk", 100);
+        Mockito.doReturn(product).when(restTemplate).getForObject("http://localhost:8189/web-market-core/api/v1/products/" + 1, ProductDto.class);
+        mvc.perform(
+                        post("/api/v1/carts/add/" + 1).contentType(MediaType.APPLICATION_JSON).content("newCart"))
+                .andDo(print())
+                .andExpect(status().isOk());
+        Cart cart = testRestTemplate.postForObject("/api/v1/carts", "newCart", Cart.class);
+        assertThat(cart.getTotalPrice()).isEqualTo(100);
+    }
+
 
 }
--- cart-service/src/test/java/com/geekbrains/spring/web/cart/CartServiceTests.java
+++ cart-service/src/test/java/com/geekbrains/spring/web/cart/CartServiceTests.java
@@ -4,6 +4,7 @@ import com.geekbrains.spring.web.cart.dto.Cart;
 import com.geekbrains.spring.web.cart.dto.OrderItemDto;
 import com.geekbrains.spring.web.cart.dto.ProductDto;
 import com.geekbrains.spring.web.cart.services.CartService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,9 @@ public class CartServiceTests {
 
     @Autowired
     private CartService cartService;
+
+    @BeforeEach
+    public void initCart() {cartService.clear("newCart");}
 
 
 
@@ -59,7 +63,23 @@ public class CartServiceTests {
         cartService.clear("newCart");
         assertThat(cartService.getCurrentCart("newCart").getTotalPrice()).isEqualTo(0);
     }
-
+    @Test
+    public void removeProductByIdToCartTest() {
+        ProductDto product = new ProductDto(1L, "Milk", 100);
+        ProductDto product2 = new ProductDto(2L, "Bread", 80);
+        Cart cart = cartService.getCurrentCart("newCart");
+        Mockito.doReturn(product).when(restTemplate).getForObject("http://localhost:8189/web-market-core/api/v1/products/" + 1, ProductDto.class);
+        Mockito.doReturn(product2).when(restTemplate).getForObject("http://localhost:8189/web-market-core/api/v1/products/" + 2, ProductDto.class);
+        cartService.addProductByIdToCart(1L,"newCart");
+        assertThat(cartService.getCurrentCart("newCart").getTotalPrice()).isEqualTo(100);
+        cartService.addProductByIdToCart(2L,"newCart");
+        assertThat(cartService.getCurrentCart("newCart").getTotalPrice()).isEqualTo(180);
+        cartService.removeProductByIdFromCart(1L,"newCart");
+        assertThat(cartService.getCurrentCart("newCart").getTotalPrice()).isEqualTo(80);
+        cartService.removeProductByIdFromCart(2L,"newCart");
+        assertThat(cartService.getCurrentCart("newCart").getTotalPrice()).isEqualTo(0);
+        cartService.removeProductByIdFromCart(1L,"newCart");
+    }
 
 
 
